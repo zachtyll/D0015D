b0b02b879b7e5a69246e321ff9d99164cda96959
Zacharias Tyllström <zache21@hotmail.se>
2020-05-04 18:32:30 +0200

    started fiddling in the js.

--- upppgift_4a/javascript/slideshow.js
+++ upppgift_4a/javascript/slideshow.js
@@ -3,12 +3,12 @@ showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("Slides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none"; 
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
+  slides[slideIndex-1].style.display = "flex"; 
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 } 
